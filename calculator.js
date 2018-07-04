00789f1002c838e7c27d78b3c6edd19999b2a0d7
nepcam <cameron.nepe@gmail.com>
2018-07-04 21:06:27 +1200

    clear function not working

--- calculator.js
+++ calculator.js
@@ -12,40 +12,40 @@ var digits = document.querySelectorAll("button")
 function addDigit(event) {
     var output = document.querySelector("#display")
     output.innerText += event.target.innerText
-    }
+}
 
     
-    // baic operators + - * /
-    function operation (){
-        entryOne.push(display.innerText)
-        operator.push(event.target.innerText)
-        display.innerText = ""
-        var operator2 = document.querySelector("#display")
-        operator2.innerText + event.target.innerText
-        }
+// baic operators + - * /
+function operation (){
+    entryOne.push(display.innerText)
+    operator.push(event.target.innerText)
+    display.innerText = ""
+    var operator2 = document.querySelector("#display")
+    operator2.innerText + event.target.innerText
+}
     
-        // solve the problem
-      function equal (operate) {
-        secondEntry.push(display.innerText)
-        var equal2 = document.querySelector("#display")
-        equal2.innerText += event.target.innerText
-        operate = operator[0]
+// retrieved from https://www.w3schools.com/jsref/jsref_parsefloat.asp
+function equal (operate) {
+    secondEntry.push(display.innerText)
+    var equal2 = document.querySelector("#display")
+    equal2.innerText += event.target.innerText
+    operate = operator[0]
         if  (operate === '+') {
-        var z= parseFloat(entryOne) + parseFloat(entryTwo)
+            var z= parseFloat(entryOne) + parseFloat(entryTwo)
         } else if (operate === '-') {
-        var z = parseFloat(entryOne) - parseFloat(entryTwo)
+            var z = parseFloat(entryOne) - parseFloat(entryTwo)
         } else if (operate === '*') {
-        var z = parseFloat(entryOne) * parseFloat(entryTwo)
+            var z = parseFloat(entryOne) * parseFloat(entryTwo)
         } else if (operate === '/') {
-        var z = parseFloat(entryOne) / parseFloat(entryTwo)
+            var z = parseFloat(entryOne) / parseFloat(entryTwo)
         } display.innerText =  z
         }
     
         // clear the display 
-      function clearDisplay () {
-        display.innerText = ""
-        display2.innerText = ""
-        entryOne.splice(0)
-        entryTwo.splice(0)
-        operator.splice(0)
-        }
+function clearDisplay () {
+    display.innerText = ""
+    display2.innerText = ""
+    entryOne.splice(0)
+    entryTwo.splice(0)
+    operator.splice(0)
+}
